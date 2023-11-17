bc5dfa5a79736419c4043c669e26fffed67dced2
MaacarenaaMbv <macarenamamolar99@gmail.com>
2023-11-17 14:21:12 +0100

    cc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AlumnosComponent } from './components/alumnos/alumnos.component';
 import { HomeComponent } from './components/home/home.component';
+import { EquipoComponent } from './components/equipo/equipo.component';
+import { EquipoService } from './services/equipos.service';
+import { AlumnosService } from './services/alumnos.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlumnosComponent
-    HomeComponent
+    AlumnosComponent,
+    HomeComponent,
+    EquipoComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [EquipoService,AlumnosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
